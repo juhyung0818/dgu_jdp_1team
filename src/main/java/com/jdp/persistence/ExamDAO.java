d0debd540d3333b47b4b272cde8a3b1851e55da0
윤주형 <juhyung0818@naver.com>
2016-10-24 19:43:00 +0900

    no message

--- src/main/java/com/jdp/persistence/ExamDAO.java
+++ src/main/java/com/jdp/persistence/ExamDAO.java
@@ -1,12 +1,14 @@
 package com.jdp.persistence;
 
 import com.jdp.domain.ExamVO;
+
 /**
  * Database Access Object
  * interface to access exam table in database
  * @author YJH
  * 2016.10.19.Wed
  */
+
 public interface ExamDAO {
 	public void register(ExamVO exam) throws Exception;
 	public void delete(ExamVO exam) throws Exception;
