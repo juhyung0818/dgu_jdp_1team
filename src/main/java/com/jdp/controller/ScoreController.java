101153877b80730b6803a7e19631a2adebed84e8
DESKTOP-TQUOAT6\soond <soonderella9>
2016-11-25 20:22:03 +0900

    temp

--- src/main/java/com/jdp/controller/ScoreController.java
+++ src/main/java/com/jdp/controller/ScoreController.java
@@ -9,12 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.jdp.domain.AverageVO;
 import com.jdp.domain.ExamVO;
@@ -152,7 +150,6 @@ public class ScoreController {
 		}
 		flag =1;
 		
-		model.addAttribute("subjectName", subjectName);
 		return "redirect:/score/management?subjectCode="+subjectCode+"&subjectName="+subjectName;
 	}
 
